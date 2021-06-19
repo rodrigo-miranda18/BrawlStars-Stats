58f4782e8fcf16b1013421831269bec631206065
rodrigo alves miranda <rodrigoalvesmiranda@yahoo.com.br>
2021-06-19 17:12:00 -0300

    Custom 404 next page

---
+++ src/components/ErrorComponent/styles.js
@@ -0,0 +1,20 @@
+import styled from "styled-components"
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+
+  width: 100%;
+  color: white;
+`
+
+export const Title = styled.span`
+  font-size: 20px;
+
+  margin: 15px 0;
+`
+
+export const Text = styled.span`
+  font-size: 14px;
+`
---
+++ src/pages/404.jsx
@@ -0,0 +1,5 @@
+import ErrorComponent from "../components/ErrorComponent"
+
+const Custom404 = () => <ErrorComponent />
+
+export default Custom404
